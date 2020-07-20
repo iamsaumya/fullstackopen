e48d1003e006a7fd9c3dd44b3397070b3523aba1
Saumya Pandey <sp160899@gmail.com>
2020-07-20 20:39:12 +0530

    Complete exercise 3.12

---
+++ part3/mongo.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length < 3) {
+  console.log('Please provide the password as an argument: node mongo.js <password>')
+  process.exit(1)
+}
+
+const password = process.argv[2]
+
+const url =
+  `mongodb+srv://saumyafullstackopen:${password}@dang-thats-delicious-qoxqy.mongodb.net/Fullstackopen?retryWrites=true&w=majority`
+
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+if(process.argv[3] && process.argv[4]){
+    const name = process.argv[3]
+    const number = process.argv[4]
+    const person = new Person({
+        name: name,
+        number: number
+      })
+      
+      person.save().then(result => {
+        console.log('note saved!')
+        mongoose.connection.close()
+      })
+}
+else{
+    Person.find({}).then(persons =>{
+        console.log('phonebook:')
+        persons.forEach(person => console.log(person.name,person.number))
+        mongoose.connection.close()
+    })
+}
