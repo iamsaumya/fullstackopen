ea282727d5c6b701944c0e0ebb5f9e8186ef04d8
Saumya Pandey <sp160899@gmail.com>
2020-07-20 18:09:41 +0530

    Update URL to backend server

--- part2/phonebook/src/services/persons.js
+++ part2/phonebook/src/services/persons.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const baseURL = "http://localhost:3001/persons"
+const baseURL = "http://localhost:3001/api/persons"
 
 const getPersons = () => {
      const request = axios.get(baseURL)
