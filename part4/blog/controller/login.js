8ad56319ae8cf555cea26d5b3969e27e3694afe1
Saumya Pandey <sp160899@gmail.com>
2020-08-04 01:27:01 +0530

    Fix async await bug

--- part4/blog/controller/login.js
+++ part4/blog/controller/login.js
@@ -6,9 +6,9 @@ const User = require("../models/user")
 loginRouter.post("/",async (request,response) => {
 	const body = request.body
 	const user = await User.findOne({ username: body.username })
-	const passwordCorrect = user === null ? false : bcrypt.compare(body.password,user.passwordHash)
+	const passwordCorrect = user === null ? false : await bcrypt.compare(body.password,user.passwordHash)
 	if(!(user && passwordCorrect)){
-		response.status(401).json({
+		return response.status(401).json({
 			error : "invalid username or password"
 		})
 	}
