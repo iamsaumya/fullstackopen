7d70b2a1d428f442d7289d29f5792cb7c1665366
Saumya Pandey <sp160899@gmail.com>
2020-08-07 01:19:16 +0530

    Complete 5.17-5.22

--- part5/bloglist-frontend/cypress/support/commands.js
+++ part5/bloglist-frontend/cypress/support/commands.js
@@ -23,3 +23,23 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
+
+Cypress.Commands.add('login', (loggingUser) => {
+    cy.request('POST','http://localhost:3001/api/login',loggingUser)
+    .then(function (response){
+        localStorage.setItem('loggedBlogUser',JSON.stringify(response.body))
+        cy.visit('http://localhost:3000')    
+    })
+  })
+  
+Cypress.Commands.add('createBlog',(body) => {
+    const headers = {
+        'Authorization' : `bearer ${JSON.parse(localStorage.getItem('loggedBlogUser')).token}`
+    }
+    cy.request({
+        method: 'POST',
+        url: 'http://localhost:3001/api/blogs',
+        body,
+        headers})
+    cy.visit('http://localhost:3000')
+})
