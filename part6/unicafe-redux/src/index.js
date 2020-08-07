4f0f53c335b4af9fdb12c4509d4de943a4c7e9f8
Saumya Pandey <sp160899@gmail.com>
2020-08-07 15:31:32 +0530

    Complete 6.1 and 6.2

---
+++ part6/unicafe-redux/src/index.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import ReactDOM from 'react-dom'
+import { createStore } from 'redux'
+import reducer from './reducer'
+
+const store = createStore(reducer)
+
+const App = () => {
+  const good = () => {
+    store.dispatch({
+      type: 'GOOD'
+    })
+  }
+  const bad = () => {
+    store.dispatch({
+      type: 'BAD'
+    })
+  }
+  const ok = () => {
+    store.dispatch({
+      type: 'OK'
+    })
+  }
+
+  const initialState = () => {
+    store.dispatch({
+      type: 'ZERO'
+    })
+  }
+
+  return (
+    <div>
+      <button onClick={good}>good</button> 
+      <button onClick={ok}>neutral</button> 
+      <button onClick={bad}>bad</button>
+      <button onClick={initialState}>reset stats</button>
+      <div>good {store.getState().good}</div>
+      <div>neutral {store.getState().ok}</div>
+      <div>bad {store.getState().bad}</div>
+    </div>
+  )
+}
+
+const renderApp = () => {
+  ReactDOM.render(<App />, document.getElementById('root'))
+}
+
+renderApp()
+store.subscribe(renderApp)
