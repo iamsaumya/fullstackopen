14d12f56d2ca23b4f7994125acab0f9102a42c21
Saumya Pandey <sp160899@gmail.com>
2020-08-16 21:34:56 +0530

    Complete 7.16

--- part7/bloglist/bloglist-frontend/src/App.js
+++ part7/bloglist/bloglist-frontend/src/App.js
@@ -1,22 +1,21 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useEffect, useRef, useState, Fragment } from "react";
 import Login from "./components/Login";
 import BlogForm from "./components/BlogForm";
 import Togglable from "./components/Togglable";
 import Notification from "./components/Notification";
 import Blogs from "./components/Blogs";
 import Users from "./components/Users";
-import User from "./components/User"
+import User from "./components/User";
 import blogService from "./services/blogs";
 import userService from "./services/users";
 
 import { showNotifcation } from "./reducers/notificationReducer";
-import { setBlogs , initializeBlogs} from "./reducers/blogReducer";
+import { setBlogs, initializeBlogs } from "./reducers/blogReducer";
 import { setLoggedUser } from "./reducers/loggedUserReducer";
-import {addUsers} from './reducers/usersReducer'
-
+import { addUsers } from "./reducers/usersReducer";
 
 import { useDispatch, useSelector } from "react-redux";
-import { Switch, Route, useRouteMatch } from "react-router-dom";
+import { Switch, Route, useRouteMatch, Link } from "react-router-dom";
 import CompleteBlog from "./components/CompleteBlog";
 
 const App = () => {
@@ -25,8 +24,14 @@ const App = () => {
 	const [users, setUsers] = useState([]);
 	const blogFormRef = useRef();
 
-	const matchUser = useRouteMatch("/users/:id")
-	const user = matchUser ? users.find(user => user.id === matchUser.params.id) : null
+	const matchUser = useRouteMatch("/users/:id");
+	const user = matchUser
+		? users.find((user) => user.id === matchUser.params.id)
+		: null;
+
+	const padding = {
+		padding: 20,
+	};
 
 	useEffect(() => {
 		const loggedBlogUser = window.localStorage.getItem("loggedBlogUser");
@@ -41,7 +46,7 @@ const App = () => {
 		(async () => {
 			const allUsers = await userService.getAll();
 			console.log(allUsers);
-			dispatch(addUsers(allUsers))
+			dispatch(addUsers(allUsers));
 			setUsers(allUsers);
 		})();
 	}, [dispatch]);
@@ -81,10 +86,10 @@ const App = () => {
 
 	const logout = () => {
 		return (
-			<div>
-				<div>{loggedInUser.name} logged in</div>
+			<React.Fragment>
+				<em>{loggedInUser.name} logged in</em>
 				<button onClick={handleLogout}>Logout</button>
-			</div>
+			</React.Fragment>
 		);
 	};
 
@@ -94,17 +99,25 @@ const App = () => {
 			{loggedInUser === null && <Login />}
 			{loggedInUser !== null && (
 				<div>
-					<h2>blogs</h2>
-					{logout()}
+					<div>
+						<Link style={padding} to="/blogs">
+							blogs
+						</Link>
+						<Link style={padding} to="/users">
+							users
+						</Link>
+						{logout()}
+					</div>
+					<h1>blog app</h1>
 					<Switch>
 						<Route path="/users/:id">
-							<User user={user}/>
+							<User user={user} />
 						</Route>
 						<Route path="/users">
 							<Users users={users} />
 						</Route>
 						<Route path="/blogs/:id">
-							<CompleteBlog/>
+							<CompleteBlog />
 						</Route>
 						<Route path="/">
 							<div>
