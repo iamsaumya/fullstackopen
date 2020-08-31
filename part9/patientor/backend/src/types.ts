c4a3caa015ed56dd07593f2ecd2537628842ffed
Saumya Pandey <sp160899@gmail.com>
2020-09-01 03:25:10 +0530

    Complete 9.19

--- part9/patientor/backend/src/types.ts
+++ part9/patientor/backend/src/types.ts
@@ -5,12 +5,50 @@ export interface DiagnosesEntry {
 }
 
 export enum Gender {
-  male = 'male',
-  female = 'female',
-  other = 'other'
+  Male = 'male',
+  Female = 'female',
+  Other = 'other'
 }
 
-export interface Entry {}
+export interface BaseEntry {
+  id: string;
+  description: string;
+  date: string;
+  specialist: string;
+  diagnosisCodes?: Array<DiagnosesEntry['code']>;
+}
+
+export enum HealthCheckRating {
+  'Healthy' = 0,
+  'LowRisk' = 1,
+  'HighRisk' = 2,
+  'CriticalRisk' = 3
+}
+
+export interface HealthCheckEntry extends BaseEntry {
+  type: 'HealthCheck';
+  healthCheckRating: HealthCheckRating;
+}
+
+export interface HospitalEntry extends BaseEntry {
+  type: 'Hospital';
+  discharge: {
+    date: string;
+    criteria: string;
+  };
+}
+export interface OccupationalHealthcareEntry extends BaseEntry {
+  type: 'OccupationalHealthcare';
+  employerName: string;
+  sickLeave?: {
+    startDate: string;
+    endDate: string;
+  };
+}
+export type Entry =
+  | HospitalEntry
+  | OccupationalHealthcareEntry
+  | HealthCheckEntry;
 
 export interface PatientEntry {
   id: string;
--- part9/patientor/backend/src/utils.ts
+++ part9/patientor/backend/src/utils.ts
@@ -1,6 +1,15 @@
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { newPatientEntry, Gender } from './types';
+import {
+  newPatientEntry,
+  Gender,
+  Entry,
+  HospitalEntry,
+  OccupationalHealthcareEntry,
+  HealthCheckEntry,
+  BaseEntry,
+  DiagnosesEntry
+} from './types';
 
 const isString = (text: any): text is string => {
   return typeof text === 'string' || text instanceof String;
@@ -19,6 +28,66 @@ const isDate = (dob: string): boolean => {
 
 const isGender = (gender: any): gender is Gender => {
   return Object.values(Gender).includes(gender);
+};
+
+const isBaseEntry = (entry: any): entry is BaseEntry => {
+  if (entry.diagnosisCodes) {
+    if (!parseDiagnosis(entry.diagnosisCodes)) {
+      throw new Error(`Incorrect Diagnosis Code ${entry.diagnosis}`);
+    }
+  }
+
+  if (
+    !entry ||
+    !isString(entry.id) ||
+    !isString(entry.description) ||
+    !isDate(entry.date) ||
+    !isString(entry.specialist)
+  ) {
+    throw new Error('Incorrect id, description, date or specialist');
+  }
+
+  return entry;
+};
+const isHospitalEntry = (entry: any): entry is HospitalEntry => {
+  if (entry.discharge) {
+    return (
+      Object.keys(entry.discharge).includes('date') &&
+      Object.keys(entry.discharge).includes('criteria')
+    );
+  }
+  return false;
+};
+
+const isOccupationalHealthcareEntry = (
+  entry: any
+): entry is OccupationalHealthcareEntry => {
+  if (entry.employerName) {
+    if (entry.sickLeave) {
+      return (
+        Object.keys(entry.sickLeave).includes('startDate') &&
+        Object.keys(entry.sickLeave).includes('endDate')
+      );
+    }
+    return true;
+  }
+  return false;
+};
+
+const isHealthCheckEntry = (entry: any): entry is HealthCheckEntry => {
+  if (
+    entry.healthCheckRating === undefined &&
+    !isString(entry.healthCheckRating)
+  ) {
+    return false;
+  }
+  return entry;
+};
+
+const parseDiagnosis = (
+  diagnosisCodes: any
+): diagnosisCodes is Array<DiagnosesEntry['code']> => {
+  return diagnosisCodes.every((diagnosisCode: any) => isString(diagnosisCode));
 };
 
 const parseDOB = (dateOfBirth: any): string => {
@@ -49,6 +118,25 @@ const parseOccupation = (occupation: any): string => {
   return occupation;
 };
 
+const parseEntry = (entries: any): Entry[] => {
+  if (!entries) {
+    throw new Error('Missing entries');
+  }
+  return entries.map((entry: any) => {
+    if (!isBaseEntry(entry)) {
+      throw new Error(`Not base entry ${entry}`);
+    }
+    if (isHospitalEntry(entry)) {
+      return entry;
+    } else if (isOccupationalHealthcareEntry(entry)) {
+      return entry;
+    } else if (isHealthCheckEntry(entry)) {
+      return entry;
+    } else {
+      throw new Error(`Not an entry from the above types.`);
+    }
+  });
+};
 // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
 const toNewPatientEntry = (object: any): newPatientEntry => {
   return {
@@ -57,7 +145,7 @@ const toNewPatientEntry = (object: any): newPatientEntry => {
     ssn: parseSSN(object.ssn),
     gender: parseGender(object.gender),
     occupation: parseOccupation(object.occupation),
-    entries: []
+    entries: parseEntry(object.entries)
   };
 };
 
